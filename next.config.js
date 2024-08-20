b3096aff445452f78a9224fa6ded426615d6c20f
Lutfil Khaliq <lutfilkhaliq587@gmail.com>
2024-08-20 19:49:22 +0800

    x

--- next.config.js
+++ next.config.js
@@ -10,7 +10,20 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 const nextConfig = {
     // output: "export",
     reactStrictMode: true,
-    images: { unoptimized: true }
+    images: { unoptimized: true },
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
 };
 
 module.exports = withPWA(nextConfig);
