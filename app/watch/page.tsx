e190175db6f7d9d0077e54ff3a485e341f26ec90
Lutfil Khaliq <lutfilkhaliq587@gmail.com>
2024-09-10 19:55:25 +0800

    bnm,

--- app/watch/page.tsx
+++ app/watch/page.tsx
@@ -3,13 +3,13 @@ import Root from "../components/root";
 import "./watch.css";
 export default async function Watch() {
 	const millisecondsPerWeek = 7 * 24 * 60 * 60 * 1000;
-	const semstartDate = new Date("2024-6-22");
+	const semstartDate = new Date("2024-9-7"); //? Note: Date shall be set every Saturday of the semester (2 Days before Mon)
 	const currentDate = new Date();
 
-	// Grab dem week
 	const _timeDifference = currentDate.getTime() - semstartDate.getTime();
 	const weekNumber = Math.ceil(_timeDifference / millisecondsPerWeek);
 	const isOdd = weekNumber % 2 !== 0;
+
 
 	return (
 		<Root className="flex items-center flex-wrap justify-center watch">
