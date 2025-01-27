1f99faca3ffca1f7bfea73a9e73e9fd7cea0b96c
Lutfil Khaliq <lutfilkhaliq587@gmail.com>
2025-01-27 08:42:47 +0800

    Edit page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Client from "./components/client/client";
 import Root from "./components/root";
+export const dynamic = 'force-dynamic' // defaults to auto
 export default async function Home() {
 	const millisecondsPerWeek = 7 * 24 * 60 * 60 * 1000;
 	const semstartDate = new Date("2025-1-4"); //? Note: Date shall be set every Saturday of the semester (2 Days before Mon)
@@ -8,13 +9,10 @@ export default async function Home() {
 	const _timeDifference = currentDate.getTime() - semstartDate.getTime();
 	const weekNumber = Math.ceil(_timeDifference / millisecondsPerWeek);
 
-	// const isOdd = weekNumber % 2 !== 0;
-	//! if you think this system is dumb, i completely agree
-
 	const HBLWeeks = [4, 10];
 	// const HBLWeeks = [2, 6, 8, 10]; //Term 2
-	const isOdd = !HBLWeeks.includes(weekNumber); // renamed to Non-HBL week
-
+	const isOdd = !HBLWeeks.includes(weekNumber); 
+	
 	return (
 		<Root className="flex items-center flex-wrap justify-center flex-col">
 			<Client
--- app/watch/page.tsx
+++ app/watch/page.tsx
@@ -1,15 +1,18 @@
 import Client from "../components/client/client";
 import Root from "../components/root";
 import "./watch.css";
+export const dynamic = 'force-dynamic' // defaults to auto
 export default async function Watch() {
 	const millisecondsPerWeek = 7 * 24 * 60 * 60 * 1000;
-	const semstartDate = new Date("2024-9-7"); //? Note: Date shall be set every Saturday of the semester (2 Days before Mon)
+	const semstartDate = new Date("2025-1-4"); //? Note: Date shall be set every Saturday of the semester (2 Days before Mon)
 	const currentDate = new Date();
 
 	const _timeDifference = currentDate.getTime() - semstartDate.getTime();
 	const weekNumber = Math.ceil(_timeDifference / millisecondsPerWeek);
-	const isOdd = weekNumber % 2 !== 0;
 
+	const HBLWeeks = [4, 10];
+	// const HBLWeeks = [2, 6, 8, 10]; //Term 2
+	const isOdd = !HBLWeeks.includes(weekNumber); 
 
 	return (
 		<Root className="flex items-center flex-wrap justify-center watch">
